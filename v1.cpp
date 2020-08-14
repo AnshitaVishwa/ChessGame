19509b8786e31c4f9bb322657f10bcf0b77ef79b
AnshitaVishwa <holaholafires@gmail.com>
2020-08-15 01:55:53 +0530

    pawn rules added

--- v1.cpp
+++ v1.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include"prettyprint.hpp"
 
 class Chess {
 private:
     char board[8][8];
     int  ux, uy, vx, vy;
+    map<pair<int, int>, set<pair<int, int>>> pawnMoves;
 public:
     Chess () {
         // Initially the board is empty
@@ -37,18 +39,50 @@ public:
         cin >> ux >> uy;
         cout << "Select the position where to move: ";
         cin >> vx >> vy;
+        if (!(pawnMoves.count({ux, uy}) and pawnMoves[{ux, uy}].count({vx, vy}))) {
+            getInput();
+        }
     }
     void boardMark () {
         board[vx][vy] = board[ux][uy];
         board[ux][uy] = ' ';
+    }
+    void pawnValidMoves () {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'B') {
+                    if (i == 6) {
+                        if (board[i - 1][j] == ' ' and board[i - 2][j] == ' ') pawnMoves[{i, j}].insert({i - 2, j});
+                    }
+                    if (i - 1 >= 0) {
+                        if (board[i - 1][j] == ' ') pawnMoves[{i, j}].insert({i - 1, j});
+                        if (j + 1 <= 7 and board[i - 1][j + 1] != ' ') pawnMoves[{i, j}].insert({i - 1, j + 1});
+                        if (j - 1 >= 0 and board[i - 1][j - 1] != ' ') pawnMoves[{i, j}].insert({i - 1, j - 1});
+                    }
+                } else if (board[i][j] == 'W') {
+                    if (i == 1) {
+                        if (board[i + 1][j] == ' ' and board[i + 2][j] == ' ') pawnMoves[{i, j}].insert({i + 2, j});
+                    }
+                    if (i + 1 <= 7) {
+                        if (board[i + 1][j] == ' ') pawnMoves[{i, j}].insert({i + 1, j});
+                        if (j + 1 <= 7 and board[i + 1][j + 1] != ' ') pawnMoves[{i, j}].insert({i + 1, j + 1});
+                        if (j - 1 >= 0 and board[i + 1][j - 1] != ' ') pawnMoves[{i, j}].insert({i + 1, j - 1});
+                    }
+                }
+            }
+        }
+        debug(pawnMoves);
     }
 };
 
 int main () {
     Chess C;
     C.displayBoard();
-    C.getInput();
-    C.boardMark();
-    C.displayBoard();
+    while(1) {
+        C.pawnValidMoves();
+        C.getInput();
+        C.boardMark();
+        C.displayBoard();
+    }
     return 0;
 }
